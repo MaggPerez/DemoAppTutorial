4cae6191fbc10b4af6417f22eba11be96cdf2e0e
maggy <maggy@localhost>
2022-05-12 12:44:19 -0400

    Added more details into this program

--- src/main/java/test/firstprogram/FirstProgram.java
+++ src/main/java/test/firstprogram/FirstProgram.java
@@ -9,14 +9,64 @@ package test.firstprogram;
  *
  * @author maggy
  */
+import java.util.Scanner;
 public class FirstProgram {
 
     public static void main(String[] args) {
+        Scanner key = new Scanner(System.in);
         System.out.println("This is a two player computer game");
-        Player p1 = new Player("John", 0);
-        Player p2 = new Player("Lee", 0);
+        
+        System.out.print("Player 1: what is your name: ");
+        String player1 = key.nextLine();
+        
+        System.out.print("Player 2: what is your name: ");
+        String player2 = key.nextLine();
+        
+        Player p1 = new Player(player1, 0);
+        Player p2 = new Player(player2, 0);
         
         System.out.println(p1);
         System.out.println(p2);
+        
+        System.out.println("\f");
+        System.out.println("In this game, you will try and match the computer to get points.");
+        
+        int turns = 0;
+        
+        while(turns < 10){
+            //Player one goes first
+            System.out.println();
+            list();
+            System.out.println("\n\nTurns: " + turns);
+            System.out.println(p1);
+            System.out.print("Player 1, choose a word: ");
+            String choice = key.next();
+            System.out.print("\t\t\t " + p1.rand(choice));
+            
+            
+            //player 2 goes next
+            System.out.println();
+            list();
+            System.out.println("\n\nTurns: " + turns);
+            System.out.println(p2);
+            System.out.print("Player 2, choose a word: ");
+            String choice2 = key.next();
+            System.out.print("\t\t\t " + p2.rand(choice2));
+            
+            //fix the score
+            
+            
+            turns++;
+        }
+    
+        
+        
+    }
+    
+    //List
+    public static void list(){
+        System.out.println("Mice, Cat, Dog, Horse");
+        
+        
     }
 }
--- src/main/java/test/firstprogram/Player.java
+++ src/main/java/test/firstprogram/Player.java
@@ -8,18 +8,45 @@ package test.firstprogram;
  *
  * @author maggy
  */
+import java.util.Random;
 public class Player {
     private String name;
     private int score;
+    private String word;
+    private String computer;
     
     public Player(String theName, int theScore){
-        name = "None";
-        score = 0;  
+        name = theName;
+        score = theScore;  
    }
     
    public void setter(String a, int num){
        name = a;
        score = num;
+   }
+   
+   //Random
+   public String rand(String x){
+       Random ran = new Random(3);
+       word = x;
+       
+       
+       
+       String words[] = {"Cat", "Mice", "Dog", "Horse"};
+       
+       int rand = (int) (Math.random() * 4);
+       computer = (words[rand]);
+       if(word.equalsIgnoreCase(computer)){
+           System.out.println("Match!");
+           score++;
+       }
+       return computer;
+   }
+   
+   
+   
+   public String toString(){
+       return(name + " " + " Current Score: " + score);
    }
    
    
